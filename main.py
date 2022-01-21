get_seg_measure calculation fixed

main.py
@@ -1,19 +1,13 @@
 import os
 import datetime
 import pathlib
-import pandas as pd
 import tensorflow as tf
 from tensorflow import keras
 from utils.general_utils import aux_funcs
-from utils.algo_utils import algo_funcs
 from utils.data_utils import data_funcs
 from utils.train_utils import train_funcs
-from utils.image_utils import image_funcs, preprocessing_funcs
-from losses import clustering_losses
-import logging
 import logging.config
 
-from augmentations import clustering_augmentations
 from configs.general_configs import (
     CONFIGS_DIR_PATH,
 )
@@ -33,22 +27,21 @@ LEARNING_RATE = 1e-4
 
 DATA_PATH = 'C:/Users/mchls/Desktop/University/PhD/projects/QANet/data/Fluo-N2DH-SIM+'
 if __name__ == '__main__':
-    parser = aux_funcs.get_arg_parcer()
+    parser = aux_funcs.get_arg_parser()
     args = parser.parse_args()
-
 
     current_run_dir = pathlib.Path(args.output_dir) / f'{TS}'
     os.makedirs(current_run_dir, exist_ok=True)
 
     logger = aux_funcs.get_logger(
-        configs_file = CONFIGS_DIR_PATH / 'logger_configs.yml',
-        save_file = current_run_dir / f'logs.log'
+        configs_file=CONFIGS_DIR_PATH / 'logger_configs.yml',
+        save_file=current_run_dir / f'logs.log'
     )
 
     if isinstance(logger, logging.Logger):
         logger.info(tf.config.list_physical_devices('GPU'))
 
-    aux_funcs.choose_gpu(gpu_id = args.gpu_id, logger=logger)
+    aux_funcs.choose_gpu(gpu_id=args.gpu_id, logger=logger)
 
     input_image_shape = (args.crop_size, args.crop_size, 1)
 
@@ -60,38 +53,47 @@ if __name__ == '__main__':
         logger=logger
     )
     if not args.no_train:
-        train_ds, val_ds = data_funcs.get_dataset_from_tiff(
-            data_dir_path=pathlib.Path(args.train_data_dir),
-            input_image_shape=input_image_shape,
-            batch_size=args.feature_extractor_batch_size,
-            validation_split=args.feature_extractor_validation_split
+        # - Train data loader
+        train_dl = data_funcs.DataLoader(
+            data=dict(),
+            crop_shape=(256, 256, 1),
+            batch_size=32,
+            shuffle=True
         )
+
+        # - Validation data loader
+        val_dl = data_funcs.DataLoader(
+            data=dict(),
+            crop_shape=(256, 256, 1),
+            batch_size=32,
+            shuffle=True
+        )
+
         train_funcs.train_model(
-            model=feat_ext_model,
+            model=model,
             data=dict(
-                train_dataset = train_ds,
-                val_dataset = val_ds,
-                X_sample = next(iter(val_ds))[0][0]
+                train_dataloader=train_dl,
+                val_dataloader=val_dl,
             ),
             callback_configs=dict(
                 output_dir_path=pathlib.Path(args.output_dir),
-                no_reduce_lr_on_plateau = args.no_reduce_lr_on_plateau_feature_extractor
+                no_reduce_lr_on_plateau=args.no_reduce_lr_on_plateau_feature_extractor
             ),
             compile_configs=dict(
-                loss = keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-                optimizer = keras.optimizers.Adam(learning_rate=args.feature_extractor_optimizer_lr),
-                metrics = ['accuracy']
+                loss=keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+                optimizer=keras.optimizers.Adam(learning_rate=args.feature_extractor_optimizer_lr),
+                metrics=['accuracy']
             ),
             fit_configs=dict(
-                batch_size = args.feature_extractor_batch_size,
-                train_epochs = args.feature_extractor_train_epochs,
-                train_steps_per_epoch = args.feature_extractor_train_steps_per_epoch,
-                validation_steps_proportion = args.feature_extractor_validation_steps_proportion,
-                valdation_freq = 1,  # [1, 100, 1500, ...] - validate on these epochs
-                shuffle = True,
+                batch_size=args.feature_extractor_batch_size,
+                train_epochs=args.feature_extractor_train_epochs,
+                train_steps_per_epoch=args.feature_extractor_train_steps_per_epoch,
+                validation_steps_proportion=args.feature_extractor_validation_steps_proportion,
+                valdation_freq=1,  # [1, 100, 1500, ...] - validate on these epochs
+                shuffle=True,
             ),
             general_configs=dict(
-                time_stamp = TS
+                time_stamp=TS
             ),
             logger=logger
         )

utils/data_utils/data_funcs.py
@@ -1,16 +1,12 @@
 import os
-import cv2
+
+import matplotlib.pyplot as plt
 import numpy as np
-import copy
-import pandas as pd
 import pathlib
 import tensorflow as tf
-from collections.abc import Callable as function
-from functools import partial
 from importlib import reload
-os.chdir('D:/University/PhD/QANET/qanet')
-from utils.general_utils import aux_funcs
-from utils.train_utils import train_funcs
+# os.chdir('D:/University/PhD/QANET/qanet')
+os.chdir('C:/Users/mchls/Desktop/University/PhD/Projects/QANet/qanet')
 from utils.image_utils import (
     image_funcs,
     augmentation_funcs,
@@ -18,9 +14,6 @@ from utils.image_utils import (
 )
 from utils.general_utils import (
     aux_funcs,
-)
-from configs.general_configs import (
-    VAL_PROP
 )
 
 
@@ -43,24 +36,25 @@ class DataLoader(tf.keras.utils.Sequence):
         self.shuff = shuffle
 
     def _get_img_seg_fls(self):
-        '''
-        > This private method pairs between the image files and their corresponding segmentations.
-        The files in the folders are asummed to have last three characters of their name
+        """
+
+        This private method pairs between the image files and their corresponding segmentations.
+        The files in the folders are assumed to have last three characters of their name
         an index which should match for all the triplets of (img, seg)
-        '''
+
+        :return: list
+        """
 
         # - All the image files
         img_fls = list()
         for root, _, image_files in os.walk(self.img_dir):
             for image_file in image_files:
-                file_name = image_file[:image_file.index('.')].split('/')[-1]
                 img_fls.append(f'{root}/{image_file}')
 
         # - All the segmentation files
         seg_fls = list()
         for root, _, seg_files in os.walk(self.seg_dir):
             for seg_file in seg_files:
-                file_name = seg_file[:seg_file.index('.')].split('/')[-1]
                 seg_fls.append(f'{root}/{seg_file}')
 
         # - Pair the image files to their segmentation files
@@ -80,16 +74,16 @@ class DataLoader(tf.keras.utils.Sequence):
         return img_seg_fls
 
     def __len__(self):
-        '''
+        """
         > Returns the number of batches
-        '''
+        """
         return int(np.floor(self.n_fls / self.btch_sz))
 
     def __getitem__(self, index):
-        '''
+        """
         > Returns a batch of in a form of tuple:
             (image crops, segmentation crops, augmented segmentation crops, target seg measure of the crops)
-        '''
+        """
         if index + self.btch_sz <= self.n_fls:
             # -1- If there are enough files to fit in the batch
             btch_fls = self.img_seg_fls[index:index+self.btch_sz]
@@ -99,19 +93,19 @@ class DataLoader(tf.keras.utils.Sequence):
         return self._get_batch(batch_files=btch_fls)
 
     def _get_batch(self, batch_files: list):
-        img_crps_btch = np.array([], dtype=np.float32)
-        seg_crps_btch = np.array([], dtype=np.float32)
-        mod_seg_crps_btch = np.array([], dtype=np.float32)
-        trgt_seg_msrs_btch = np.array([], dtype=np.float32)
+        img_crps_btch = list()
+        seg_crps_btch = list()
+        mod_seg_crps_btch = list()
+        trgt_seg_msrs_btch = list()
 
         # 1) For each file in the batch files
         btch_fls = list(map(aux_funcs.decode_file, batch_files))
         for idx, (img_fl, seg_fl) in enumerate(btch_fls):
             # 2) Read the image
-            img = image_funcs.load_image(img_fl)  #cv2.imread(img_fl, cv2.IMREAD_UNCHANGED)
-            print(img.shape)
-            seg = image_funcs.load_image(seg_fl)  #cv2.imread(seg_fl, cv2.IMREAD_UNCHANGED)
-            print(seg.shape)
+            img = image_funcs.load_image(img_fl)  # cv2.imread(img_fl, cv2.IMREAD_UNCHANGED)
+            # print(img.shape)
+            seg = image_funcs.load_image(seg_fl)  # cv2.imread(seg_fl, cv2.IMREAD_UNCHANGED)
+            # print(seg.shape)
 
             # 3) Apply the preprocessing function
             img = preprocessing_funcs.preprocessings(image=img)
@@ -124,23 +118,28 @@ class DataLoader(tf.keras.utils.Sequence):
             )
             mod_seg_crp = augmentation_funcs.augmentations(seg_crp)
             trgt_seg_msr = aux_funcs.get_seg_measure(
-                ground_truth_segmentations=seg_crp,
-                predicted_segmentations=mod_seg_crp
+                ground_truth=seg_crp,
+                segmentations=mod_seg_crp
             )
 
-            img_crps_btch = np.append(img_crps_btch, img_crp)
-            seg_crps_btch = np.append(seg_crps_btch, seg_crp)
-            mod_seg_crps_btch = np.append(mod_seg_crps_btch, mod_seg_crp)
-            trgt_seg_msrs_btch = np.append(trgt_seg_msrs_btch, trgt_seg_msr)
+            img_crps_btch.append(img_crp)
+            seg_crps_btch.append(seg_crp)
+            mod_seg_crps_btch.append(mod_seg_crp)
+            trgt_seg_msrs_btch.append(trgt_seg_msr)
 
-        if self.shuffle:
-            rand_idxs = np.arange(img_btch.shape[0])
-            np.random.shuffle(rand_idxs)
+        img_crps_btch = np.array(img_crps_btch, dtype=np.float32)
+        seg_crps_btch = np.array(seg_crps_btch, dtype=np.float32)
+        mod_seg_crps_btch = np.array(mod_seg_crps_btch, dtype=np.float32)
+        trgt_seg_msrs_btch = np.array(trgt_seg_msrs_btch, dtype=np.float32)
 
-            img_crps_btch = img_crps_btch[random_idxs]
-            seg_crps_btch = seg_crps_btch[random_idxs]
-            mod_seg_crps_btch = mod_seg_crps_btch[random_idxs]
-            trgt_seg_msrs_btch = trgt_seg_msrs_btch[random_idxs]
+        if self.shuff:
+            rnd_idxs = np.arange(img_crps_btch.shape[0])
+            np.random.shuffle(rnd_idxs)
+
+            img_crps_btch = img_crps_btch[rnd_idxs]
+            seg_crps_btch = seg_crps_btch[rnd_idxs]
+            mod_seg_crps_btch = mod_seg_crps_btch[rnd_idxs]
+            trgt_seg_msrs_btch = trgt_seg_msrs_btch[rnd_idxs]
 
         return (
             tf.convert_to_tensor(img_crps_btch, dtype=tf.float32),
@@ -150,25 +149,32 @@ class DataLoader(tf.keras.utils.Sequence):
         )
 
 
-DATA_DIR = pathlib.Path('D:/University/PhD/QANET/Data/Fluo-N2DH-GOWT1-ST')
+# DATA_DIR = pathlib.Path('D:/University/PhD/QANET/Data/Fluo-N2DH-GOWT1-ST')
+DATA_DIR = pathlib.Path('C:/Users/mchls/Desktop/University/PhD/Projects/QANet/Data/Silver_GT/Fluo-N2DH-GOWT1-ST')
 IMAGE_DIR = DATA_DIR / '01'
 IMAGE_DIR.is_dir()
 GT_DIR = DATA_DIR / '01_ST/SEG'
 GT_DIR.is_dir()
 
-if __name__=='__main__':
-    data = dict(
+if __name__ == '__main__':
+    test_data = dict(
         images_dir=IMAGE_DIR,
         segmentations_dir=GT_DIR
     )
-    reload(preprocessing_funcs)
-    reload(image_funcs)
+    reload(aux_funcs);
+    reload(preprocessing_funcs);
+    reload(image_funcs);
     dl = DataLoader(
-        data=data,
+        data=test_data,
         crop_shape=(254, 254, 1),
-        batch_size=32,
+        batch_size=10,
         shuffle=True
     )
-
-    for btch in dl:
-        print(btch.shape)
+    # len(dl)
+    np.count_nonzero(np.array([1, 2, 3]))
+    for idx, btch in enumerate(dl):
+        imgs, segs, mod_segs, J = btch
+        print(J)
+        plt.imshow(imgs[0], cmap='gray')
+        # print(idx)
+        # print(btch[0].shape)

utils/general_utils/aux_funcs.py
@@ -1,20 +1,17 @@
 import os
 import yaml
 import logging
+import logging.config
 import threading
 import argparse
 import pathlib
 import tensorflow as tf
 import numpy as np
-import pandas as pd
-from sklearn.neighbors import NearestNeighbors
 from models import cnn
-from utils.image_utils import image_funcs
 from configs.general_configs import (
     TRAIN_DATA_DIR,
     TEST_DATA_DIR,
     OUTPUT_DIR,
-    CONFIGS_DIR_PATH,
     EPSILON,
 )
 
@@ -79,7 +76,7 @@ def get_model(checkpoint_dir: pathlib.Path = None, logger: logging.Logger = None
                 logger.exception(err)
         else:
             if isinstance(logger, logging.Logger):
-                logger.info(f'Weights from \'checkpoint_dir\' were loaded successfully to the \'{model_name}\' model!')
+                logger.info(f'Weights from \'checkpoint_dir\' were loaded successfully to the \'RibCage\' model!')
 
     if isinstance(logger, logging.Logger):
         logger.info(model.summary())
@@ -131,7 +128,7 @@ def get_logger(configs_file, save_file):
     return logger
 
 
-def get_arg_parcer():
+def get_arg_parser():
     parser = argparse.ArgumentParser()
 
     # FLAGS
@@ -146,7 +143,7 @@ def get_arg_parcer():
 
     # c) Network
     parser.add_argument('--train_epochs', type=int, default=100, help='Number of epochs to train the feature extractor network')
-    parser.add_argument('--train_steps_per_epoch', type=int, default=1000, help='Number of iterations that will be performed on each epoch for the featrue extractor network')
+    parser.add_argument('--train_steps_per_epoch', type=int, default=1000, help='Number of iterations that will be performed on each epoch for the feature extractor network')
     parser.add_argument('--batch_size', type=int, default=32, help='The number of samples in each batch')
     parser.add_argument('--validation_split', type=float, default=0.1, help='The proportion of the data to be used for validation in the train process of the feature extractor model ((should be in range [0.0, 1.0])')
     parser.add_argument('--validation_steps_proportion', type=float, default=0.5, help='The proportion of validation steps in regards to the training steps in the train process of the feature extractor model ((should be in range [0.0, 1.0])')
@@ -159,17 +156,17 @@ def get_arg_parcer():
 
 
 def get_jaccard(gt_batch, seg_batch):
-    '''
+    """
     Receives two batches one of the predicted segmentations and the other of the
     ground truth segmentations, and returns the Jaccard measure i.e., the intersection
     over union (J = I / U)
     :gt_batch: The ground truth segmentation batch in format NHWC
     :seg_batch: The predicted segmentation batch in format NHWC
     :return:
-        1) J - np.array of jaccard measurs
+        1) J - np.array of jaccard measures
         2) I - np.array of the intersection quantities
         3) U - np.array of the union quantities
-    '''
+    """
     # - Transform the gt_batch image into a binary format
     if np.max(gt_batch) > 1:
         gt_batch = np.array(gt_batch > 0, dtype=np.int16)
@@ -184,115 +181,68 @@ def get_jaccard(gt_batch, seg_batch):
     # - Calculate the union of the images
     U = np.logical_or(gt_batch, seg_batch).sum(axis=(1, 2))
 
-    # - Calculat the Jaccard coefficient
+    # - Calculate the Jaccard coefficient
     J = I / (U + EPSILON)
 
     return J, I, U
 
 
-def get_seg_measure(ground_truth_segmentations, predicted_segmentations):
-    labels = np.where(np.unique(ground_truth_segmentations) > 0)[0]  # Exclude the background
-    print('labels:', labels)
+def get_seg_measure(ground_truth, segmentations):
+    labels = np.unique(ground_truth)[1:]  # Exclude the background (the '0')
+
     number_of_classes = labels.shape[0]
+
     gt_areas = np.array([])
-    intersect_areas = np.array([])
+    I_areas = np.array([])
     obj_jaccards = np.array([])
+
     for gt_cls in labels:
-        print('gt_cls:', gt_cls)
         # - Prepare the ground truth label
-        gt_lbl = np.zeros_like(ground_truth_segmentations)
-        gt_lbl_px = np.argwhere(ground_truth_segmentations == gt_cls)
-        print('gt:', ground_truth_segmentations[ground_truth_segmentations==gt_cls])
-        print('gt_lbl_px', gt_lbl_px)
-        gt_x, gt_y = gt_lbl_px[:, 0], gt_lbl_px[:, 1]
-        gt_lbl[(gt_x, gt_y)] = 1
-        gt_areas = np.append(gt_areas, gt_lbl.sum())
+        gt_lbl = np.zeros_like(ground_truth)
+        gt_lbl_px = np.argwhere(ground_truth == gt_cls)
+        if gt_lbl_px.shape[0]:
+            gt_x, gt_y = gt_lbl_px[:, 0], gt_lbl_px[:, 1]
+            gt_lbl[(gt_x, gt_y)] = 1  # => mark the entries at the indices as '1', to produce a binary label
+            gt_areas = np.append(gt_areas, gt_lbl.sum())
 
-        # - Prepare the ground truth label
-        # (*) As the predicted segmentations' class may differ, we need to infere it
-        # from the ground truth, by looking at the most abundant label in the vacinity
-        # of the ground truth label
-        I = gt_lbl * predicted_segmentations  # => provides the labels which correspond to the ground truth label
-        I = I[I > 0]  # => as '0' will be there, we need to dispose of it, as it is the background
-        seg_lbls, cnts = np.unique(I, return_counts=True)  # => counting the labels
-        seg_cls = seg_lbls[np.argmax(cnts)]  # => here we choose the most abundant label as the class we are looking for in the predictions
+            # - Prepare the ground truth label
+            # (*) As the predicted segmentations' class may differ, we need to infer it
+            # from the ground truth, by looking at the most abundant label in the vicinity
+            # of the ground truth label
+            I = gt_lbl * segmentations  # => provides the labels which correspond to the ground truth label
+            I = I[I > 0]  # => as '0' will be there, we need to dispose of it, as it is the background
 
-        seg_lbl = np.zeros_like(predicted_segmentations)
-        seg_lbl_px = np.argwhere(predicted_segmentations == seg_cls)
-        print('seg:', predicted_segmentations[predicted_segmentations==seg_cls])
-        seg_x, seg_y = seg_lbl_px[:, 0], seg_lbl_px[:, 1]
-        seg_lbl[(seg_x, seg_y)] = 1
+            seg_lbls, cnts = np.unique(I, return_counts=True)  # => counting the labels
 
-        # - Calculate the intersection of the ground truth segmentation with
-        # the predicted segmentation
-        I_area = np.logical_and(gt_lbl, seg_lbl).sum()
-        print('I:', I_area)
-        intersect_areas = np.append(intersect_areas, I_area)
+            # - If the predicted label does not contain the class - skip it
+            if not cnts.any():
+                I_areas = np.append(I_areas, 0.)  # => add the intersection area, which in this case is 0
+                obj_jaccards = np.append(obj_jaccards, 0.)  # => add the jaccard coefficient, which in this case is also 0
+                continue
 
-        # - Calculate the union of the ground truth segmentation with
-        # the predicted segmentation
-        U_area = np.logical_or(gt_lbl, seg_lbl).sum()
-        print('U:', U_area)
+            seg_cls = seg_lbls[np.argmax(cnts)]  # => here we choose the most abundant label as the class we are looking for in the predictions
 
-        # - Calculate the Jaccard coefficient of the current label
-        J = I_area / (U_area + EPSILON)
-        obj_jaccards = np.append(obj_jaccards, J)
-    print('obj_jacc:', obj_jaccards)
-    print('gt_areas:', gt_areas)
+            seg_lbl = np.zeros_like(segmentations)
+            seg_lbl_px = np.argwhere(segmentations == seg_cls)
+            seg_x, seg_y = seg_lbl_px[:, 0], seg_lbl_px[:, 1]
+            seg_lbl[(seg_x, seg_y)] = 1
+            I_area = np.logical_and(gt_lbl, seg_lbl).sum()  # => calculate the intersection of the ground truth with the segmentation
+            I_areas = np.append(I_areas, I_area)
+
+            # - Calculate the union of the ground truth segmentation with
+            # the predicted segmentation
+            U_area = np.logical_or(gt_lbl, seg_lbl).sum()
+
+            # - Calculate the Jaccard coefficient of the current label
+            J = I_area / (U_area + EPSILON)
+            obj_jaccards = np.append(obj_jaccards, J)
+
     # - The detection counts only if the intersection is greater then INTERSECTION_TH
-    dets = intersect_areas / (gt_areas + EPSILON) > DETECTION_TH
-    print('dets:', dets)
+    dets = I_areas / (gt_areas + EPSILON) > .5
+
     # - The seg measure is the sum of the jaccards of the detected objects to
     # the number of different objects in the image
+
     seg_measure = obj_jaccards[dets].sum() / (number_of_classes + EPSILON)
 
     return seg_measure
-
-# def get_seg_measure(gt_batch, seg_batch):
-#     btch_seg_measures = np.array([])
-#     for idx, (gt, seg) in enumerate(zip(gt_batch, seg_batch)):
-#         labels = np.where(np.unique(gt) > 0)  # Exclude the background
-#         number_of_classes = labels.shape[0]
-#         gt_areas = np.array([])
-#         intersect_areas = np.array([])
-#         jaccards = np.array([])
-#         for lbl in labels:
-#             # - Prepare the ground truth label
-#             gt_lbl = np.zeros_like(gt)
-#             gt_lbl_px = np.argwhere(gt_lbl == lbl)
-#             gt_x, gt_y = gt_lbl_px[:, 0], gt_lbl_px[:, 1]
-#             gt_lbl[(gt_x, gt_y)] = 1
-#             gt_areas = np.append(gt_areas, gt_lbl.sum())
-#
-#             # - Prepare the ground truth label
-#             seg_lbl = np.zeros_like(seg)
-#             seg_lbl_px = np.argwhere(seg_lbl == lbl)
-#             seg_x, seg_y = seg_lbl_px[:, 0], seg_lbl_px[:, 1]
-#             seg_lbl[(seg_x, seg_y)] = 1
-#
-#             # - Calculate the intersection of the ground truth segmentation with
-#             # the predicted segmentation
-#             I = np.logical_and(gt_lbl, seg_lbl)
-#             intersect_areas = np.append(intersect_areas, I.sum())
-#
-#             # - Calculate the union of the ground truth segmentation with
-#             # the predicted segmentation
-#             U = np.logical_or(gt_lbl, seg_lbl)
-#
-#             # - Calculate the Jaccard coefficient of the current label
-#             J = I / (U + EPSILON)
-#             jaccards = np.append(jaccards, J)
-#
-#         # - The detection counts only if the intersection is greater then INTERSECTION_TH
-#         dets = intersect_areas / (gt_areas + EPSILON) > DETECTION_TH
-#
-#         # - The seg measure is the sum of the jaccards of the detected objects to
-#         # the number of different objects in the image
-#         seg_measure = jaccards[dets].sum() / (number_of_classes + EPSILON)
-#
-#         btch_seg_measures = np.append(btch_seg_measures, seg_measure)
-#
-#     return seg_measure
-#
-#
-#

utils/image_utils/preprocessing_funcs.py
@@ -11,8 +11,8 @@ def preprocessings(image):
     img = image
     if len(img.shape) < 3:
         img = np.expand_dims(img, 2)
-    print(img.shape)
-    
+    # print(img.shape)
+
     # - If the image is in RGB - convert it to gray scale
     if img.shape[-1] > 1:
         img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)