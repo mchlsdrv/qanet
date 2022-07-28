Jaccard calculation in pytorch module fixed

pytorch/custom/augs.py
@@ -1,5 +1,3 @@
-import time
-
 import albumentations as A
 import albumentations.augmentations.transforms as tr
 import numpy as np
@@ -9,9 +7,10 @@ from scipy.ndimage import (
     grey_dilation,
     grey_erosion
 )
+import matplotlib.pyplot as plt
 from scipy.ndimage.filters import gaussian_filter
 from scipy.ndimage.interpolation import map_coordinates
-
+from PIL import Image
 from configs.general_configs import (
     EROSION_SIZES,
     DILATION_SIZES,
@@ -22,6 +21,9 @@ from configs.general_configs import (
 )
 
 __author__ = 'sidorov@post.bgu.ac.il'
+
+from utils.aux_funcs import calc_jaccard
+from utils.data_utils import scan_files
 
 
 def random_erosion(mask, **kwargs):
@@ -56,7 +58,7 @@ def elastic_transform(mask, **kwargs):
     Proc. of the International Conference on Document Analysis and
     Recognition, 2003.
     """
-    t_strt = time.time()
+    mask = np.transpose(mask, (1, 2, 0))
     seg_shp = mask.shape
     rnd_st = np.random.RandomState(None)
 
@@ -67,9 +69,12 @@ def elastic_transform(mask, **kwargs):
     dy = gaussian_filter((rnd_st.rand(*seg_shp) * 2 - 1), sgm) * alph
 
     x, y = np.meshgrid(np.arange(seg_shp[0]), np.arange(seg_shp[1]))
-    idxs = np.reshape(y + dy, (-1, 1)), np.reshape(x + dx, (-1, 1))
+    idxs = np.reshape(np.expand_dims(y, -1) + dy, (-1, 1)), np.reshape(np.expand_dims(x, -1) + dx, (-1, 1))
 
-    mask = map_coordinates(mask, idxs, order=1, mode='reflect').reshape(seg_shp)
+    mask = map_coordinates(np.reshape(mask, (-1, 1)), idxs, order=1, mode='reflect').reshape(seg_shp)
+
+    mask = np.transpose(mask, (2, 0, 1))
+
     return mask
 
 
@@ -187,3 +192,38 @@ def inference_augs():
             ToTensorV2()
         ]
     )
+
+
+def check_augs(data_dir, ):
+    files = scan_files(root_dir=data_dir, seg_dir_postfix='GT', image_prefix='t0', seg_prefix='man_seg0')
+    augs = mask_augs()
+
+    imgs = []
+    masks = []
+    mask_augs = []
+    jaccards = []
+    for img_fl, seg_fl in files:
+        img = np.array(Image.open(str(img_fl)).convert('L'), dtype=np.float32)
+        mask = np.array(Image.open(str(seg_fl)).convert('L'), dtype=np.float32)
+
+        aug_res = augs(image=img, mask=mask)
+        img_aug, mask_aug = aug_res.get('image'), aug_res.get('mask')
+
+        jaccard = calc_jaccard(mask, mask_aug)
+
+        imgs.append(img)
+        masks.append(mask)
+        mask_augs.append(mask_aug)
+        jaccards.append(jaccard)
+
+    imgs = np.array(imgs)
+    masks = np.array(masks)
+    mask_augs = np.array(mask_augs)
+    jaccards = np.array(jaccards)
+
+    for idx in range(10):
+        fig, ax = plt.subplots(1, 3)
+        ax[0].imshow(imgs[idx], cmap='gray')
+        ax[1].imshow(masks[idx], cmap='gray')
+        ax[2].imshow(mask_augs[idx], cmap='gray')
+        fig.suptitle(f'Jaccard: {jaccards[idx]:.3f}')

pytorch/utils/aux_funcs.py
@@ -3,28 +3,20 @@ import logging.config
 import pickle as pkl
 from functools import partial
 
-import albumentations as A
 import torchvision
 import yaml
-from albumentations.pytorch import ToTensorV2
 from utils.logging_funcs import info_log, err_log
 
 from configs.general_configs import (
     OUTPUT_DIR,
 
     EPOCHS,
-    BATCH_SIZE,
+    TRAIN_BATCH_SIZE,
     OPTIMIZER_LR,
 
     OPTIMIZER,
     OPTIMIZER_WEIGHT_DECAY,
     OPTIMIZER_MOMENTUM_DECAY,
-
-    KERNEL_REGULARIZER_TYPE,
-    KERNEL_REGULARIZER_L1,
-    KERNEL_REGULARIZER_L2,
-    KERNEL_REGULARIZER_FACTOR,
-    KERNEL_REGULARIZER_MODE,
 
     CHECKPOINT_DIR,
     CHECKPOINT_FILE,
@@ -78,33 +70,6 @@ def save_checkpoint(state, filename='my_checkpoint.pth.tar'):
 def load_checkpoint(checkpoint, model):
     print('=> Loading checkpoint')
     model.load_state_dict(checkpoint['state_dict'])
-
-
-def get_accuracy(data_loader, model, device='cuda'):
-    num_correct = 0
-    num_pixels = 0
-    dice_score = 0.0
-    model.eval()
-
-    with torch.no_grad():
-        for img, seg, seg_aug, seg_msr in data_loader:
-            x = x.to(device=device)
-            y = y.to(device='cpu').unsqueeze(1)
-
-            preds = torch.sigmoid(model(x)).to('cpu')
-            preds = (preds > 0.5).float()
-
-            num_correct += (preds == y).sum()
-            num_pixels += torch.numel(preds)
-            acc = (num_correct / num_pixels)
-            dice_score += (2 * (preds * y).sum()) / ((preds + y).sum() + 1e-8)
-    dice_score = dice_score / len(data_loader)
-    print(f'Accuracy: {acc * 100 :.4f}%')
-    print(f'Dice Score: {dice_score:.4f}')
-
-    model.train()
-
-    return acc, dice_score
 
 
 def save_preds(data_loader, model, save_dir='./results/images', device='cuda'):
@@ -178,26 +143,72 @@ def get_runtime(seconds: float):
     return hrs_str + ':' + min_str + ':' + sec_str + '[H:M:S]'
 
 
-def get_augs(args: dict):
-    train_augs = A.Compose([
-        A.Resize(height=args.get('image_height'), width=args.get('image_width')),
-        A.Rotate(limit=35, p=1.0),
-        A.HorizontalFlip(p=0.5),
-        A.VerticalFlip(p=0.1),
-        A.CLAHE(p=1.),
-        A.ToFloat(p=1.),
-        ToTensorV2()
-    ])
+def calc_jaccard(R: np.ndarray, S: np.ndarray):
+    """
+    Calculates the mean Jaccard coefficient for two multi-class labels
+    :param: R - Reference multi-class label
+    :param: S - Segmentation multi-class label
+    """
+    def _get_one_hot_masks(multi_class_mask: np.ndarray, classes: np.ndarray = None):
+        """
+        Converts a multi-class label into a one-hot labels for each object in the multi-class label
+        :param: multi_class_mask - mask where integers represent different objects
+        """
+        # - Ensure the multi-class label is populated with int values
+        mlt_cls_mask = multi_class_mask.astype(np.int16)
 
-    val_augs = A.Compose([
-        A.Resize(height=args.get('image_height'), width=args.get('image_width')),
-        A.CLAHE(p=1.),
-        A.ToFloat(p=1.),
-        ToTensorV2()
-    ])
+        # - Find the classes
+        cls = classes
+        if cls is None:
+            cls = np.unique(mlt_cls_mask)
 
-    return train_augs, val_augs
+        # - Discard the background (0)
+        cls = cls[cls > 0]
 
+        one_hot_masks = np.zeros((len(cls), *mlt_cls_mask.shape), dtype=np.float32)
+        for lbl_idx, lbl in enumerate(one_hot_masks):
+            # for obj_mask_idx, _ in enumerate(lbl):
+            idxs = np.argwhere(mlt_cls_mask == cls[lbl_idx])
+            x, y = idxs[:, 0], idxs[:, 1]
+            one_hot_masks[lbl_idx, x, y] = 1.
+        return one_hot_masks
+
+    # - In case theres no other classes besides background (i.e., 0) J = 0.
+    J = 0.
+    R = np.array(R)
+    S = np.array(S)
+    # - Convert the multi-label mask to multiple one-hot masks
+    cls = np.unique(R.astype(np.int16))
+
+    if cls[cls > 0].any():  # <= If theres any classes besides background (i.e., 0)
+        R = _get_one_hot_masks(multi_class_mask=R, classes=cls)
+        S = _get_one_hot_masks(multi_class_mask=S, classes=cls)
+
+        # - Calculate the intersection of R and S
+        I_sums = np.sum(R[:, np.newaxis, ...] * S[np.newaxis, ...], axis=(-2, -1))
+        x, y = np.arange(len(I_sums)), np.argmax(I_sums, axis=1)  # <= Choose the once that have the largest overlap with the ground truth label
+        I = I_sums[x, y]
+
+        # - Calculate the union of R and S
+        U_sums = np.sum(np.logical_or(R[:, np.newaxis, ...], S[np.newaxis, ...]), axis=(-2, -1))
+        U = U_sums[x, y]
+
+        # - Mean Jaccard on the valid items only
+        U[U <= 0] = 1  # <= To avoid division by 0
+        J = (I / U)
+
+        # - Calculate the areas of the reference items
+        R_areas = R.sum(axis=(-2, -1))
+        R_areas[R_areas <= 0] = 1  # <= To avoid division by 0
+
+        # - Find out the indices of the items which do not satisfy |I| / |R| > 0.5 and replace them with 0
+        inval = np.argwhere((I / R_areas) <= .5).reshape(-1)
+
+        J[inval] = np.nan
+
+        J = np.nan_to_num(J, copy=True, nan=0.0, posinf=0.0, neginf=0.0)
+
+    return J if isinstance(J, float) else J.mean()
 
 def get_optimizer(params, algorithm: str, args: dict):
     optimizer = None
@@ -344,7 +355,7 @@ def get_arg_parser():
 
     # - TRAINING
     parser.add_argument('--epochs', type=int, default=EPOCHS, help='Number of epochs to train the model')
-    parser.add_argument('--batch_size', type=int, default=BATCH_SIZE, help='The number of samples in each batch')
+    parser.add_argument('--batch_size', type=int, default=TRAIN_BATCH_SIZE, help='The number of samples in each batch')
     parser.add_argument('--num_workers', type=int, default=NUM_WORKERS, help='The number of workers to load the data')
     parser.add_argument('--val_prop', type=float, default=VAL_PROP, help=f'The proportion of the data which will be set aside, and be used in the process of validation')
     parser.add_argument('--checkpoint_file', type=str, default=CHECKPOINT_FILE, help=f'The path to the file which contains the checkpoints of the model')
@@ -367,18 +378,6 @@ def get_arg_parser():
     parser.add_argument('--optimizer_nesterov', default=False, action='store_true', help=f'If to use the Nesterov momentum (SGD)')
     parser.add_argument('--optimizer_centered', default=False, action='store_true', help=f'If True, gradients are normalized by the estimated variance of the gradient; if False, by the un-centered second moment. Setting this to True may help with training, but is slightly more expensive in terms of computation and memory. (RMSprop)')
 
-    # - CALLBACKS
-    parser.add_argument('--no_drop_block', default=False, action='store_true', help=f'If to use the drop_block in the network')
-    parser.add_argument('--drop_block_keep_prob', type=float, help=f'The probability to keep the block')
-    parser.add_argument('--drop_block_block_size', type=int, help=f'The size of the block to drop')
-
-    parser.add_argument('--kernel_regularizer_type', type=str, choices=['l1', 'l2', 'l1l2'], default=KERNEL_REGULARIZER_TYPE, help=f'The type of the regularization')
-    parser.add_argument('--kernel_regularizer_l1', type=float, default=KERNEL_REGULARIZER_L1, help=f'The strength of the L1 regularization')
-    parser.add_argument('--kernel_regularizer_l2', type=float, default=KERNEL_REGULARIZER_L2, help=f'The strength of the L2 regularization')
-    parser.add_argument('--kernel_regularizer_factor', type=float, default=KERNEL_REGULARIZER_FACTOR, help=f'The strength of the orthogonal regularization')
-    parser.add_argument('--kernel_regularizer_mode', type=str, choices=['rows', 'columns'], default=KERNEL_REGULARIZER_MODE, help=f"The mode ('columns' or 'rows') of the orthogonal regularization")
-
-    parser.add_argument('--wandb', default=False, action='store_true', help=f'If to use the Weights and Biases board')
     parser.add_argument('--load_model', default=False, action='store_true', help=f'If to load the model')
 
     return parser
@@ -543,7 +542,7 @@ def train_model(data_dir, epochs, args, device: str, save_dir: pathlib.Path, log
     # - Get the data loaders
     train_data_loader, val_data_loader, _ = get_data_loaders(
         data_dir=data_dir,
-        batch_size=args.batch_size,
+        train_batch_size=args.batch_size,
         val_prop=VAL_PROP,
         train_augs=train_augs,
         val_augs=val_augs,
@@ -637,6 +636,7 @@ def train_model(data_dir, epochs, args, device: str, save_dir: pathlib.Path, log
                 val - {val_true_seg_msr:.3f} / {val_pred_seg_msr:.3f} ({100 - (val_pred_seg_msr * 100 / (val_true_seg_msr + EPSILON)):.2f}%)
         ''')
 
+        # - CALLBACKS
         # > Early Stopping
         if EARLY_STOPPING:
             if no_imprv_epchs >= EARLY_STOPPING_PATIENCE:
@@ -662,7 +662,7 @@ def train_model(data_dir, epochs, args, device: str, save_dir: pathlib.Path, log
 def test_model(model, data_dir, args, device: str, seg_dir_postfix: str, image_prefix: str, seg_prefix: str, save_dir: pathlib.Path, logger: logging.Logger = None):
     _, _, test_data_loader = get_data_loaders(
         data_dir=data_dir,
-        batch_size=1,
+        train_batch_size=0,
         train_augs=None,
         val_augs=None,
         seg_dir_postfix=seg_dir_postfix,

pytorch/utils/data_utils.py
@@ -1,5 +1,6 @@
 import os
 
+import torch
 from PIL import Image
 from torch.utils.data import Dataset, DataLoader
 import re
@@ -8,14 +9,10 @@ import numpy as np
 import pathlib
 import logging
 
-from configs.general_configs import SEG_DIR_POSTFIX, IMAGE_PREFIX, SEG_PREFIX
+from configs.general_configs import SEG_DIR_POSTFIX, IMAGE_PREFIX, SEG_PREFIX, VAL_BATCH_SIZE, TEST_BATCH_SIZE, NUM_WORKERS
 from custom.augs import (
     mask_augs,
 )
-
-# from utils.torch_image_funcs import (
-#     get_contours,
-# )
 
 from utils.logging_funcs import (
     info_log,
@@ -32,35 +29,6 @@ class ImageDS(Dataset):
         self.augs = augs
         self.mask_augs = mask_augs()
 
-    # def _clean_blanks(self):
-    #     """
-    #     Cleans all the images there theres no data, i.e., blank or only random noise
-    #     """
-    #     clean_data = []
-    #     for idx, (img, seg) in enumerate(self.imgs_segs):
-    #         bin_seg = copy(seg)
-    #
-    #         # - Find the indices of the current labels' class in the segmentation label
-    #         seg_px = np.arguer(bin_seg > 0)
-    #
-    #         # - Separate the indices into x and y coordinates
-    #         seg_pix_xs, seg_pix_ys = seg_px[:, 0], seg_px[:, 1]
-    #
-    #         # - Mark the entries at the indices that correspond to the label as '1', to produce a binary label
-    #         bin_seg[(seg_pix_xs, seg_pix_ys)] = 1
-    #
-    #         # - Images should be of type UINT8
-    #         bin_seg = bin_seg.astype(np.uint8)
-    #
-    #         # - Find the contours in the image
-    #         _, centroids = get_contours(image=bin_seg)
-    #
-    #         # - If the image has at least one contour (i.e., it is not blank or noise) - add it to the data
-    #         if centroids:
-    #             clean_data.append((img, seg))
-    #
-    #     self.imgs_segs = np.array(clean_data)
-
     def __len__(self):
         return len(self.file_tuples)
 
@@ -75,21 +43,77 @@ class ImageDS(Dataset):
         mask_aug_res = self.mask_augs(image=np.array(img), mask=np.array(mask))
         mask_aug = mask_aug_res.get('mask')
 
-        jaccard = get_jaccard(mask, mask_aug)
+        jaccard = calc_jaccard(R=mask, S=mask_aug)
 
-        return img, mask, mask_aug, jaccard
+        return img, mask, mask_aug, torch.tensor(jaccard, dtype=torch.float)
 
 
-def get_jaccard(gt, pred):
+def calc_jaccard(R: np.ndarray, S: np.ndarray):
+    """
+    Calculates the mean Jaccard coefficient for two multi-class labels
+    :param: R - Reference multi-class label
+    :param: S - Segmentation multi-class label
+    """
+    def _get_one_hot_masks(multi_class_mask: np.ndarray, classes: np.ndarray = None):
+        """
+        Converts a multi-class label into a one-hot labels for each object in the multi-class label
+        :param: multi_class_mask - mask where integers represent different objects
+        """
+        # - Ensure the multi-class label is populated with int values
+        mlt_cls_mask = multi_class_mask.astype(np.int16)
 
-    gt[gt > 0] = 1
-    pred[pred > 0] = 1
+        # - Find the classes
+        cls = classes
+        if cls is None:
+            cls = np.unique(mlt_cls_mask)
 
-    I = np.logical_and(gt, pred)
-    U = np.logical_and(gt, pred)
-    J = I.sum() / (U.sum() + 1e-8)
+        # - Discard the background (0)
+        cls = cls[cls > 0]
 
-    return J
+        one_hot_masks = np.zeros((len(cls), *mlt_cls_mask.shape), dtype=np.float32)
+        for lbl_idx, lbl in enumerate(one_hot_masks):
+            # for obj_mask_idx, _ in enumerate(lbl):
+            idxs = np.argwhere(mlt_cls_mask == cls[lbl_idx])
+            x, y = idxs[:, 0], idxs[:, 1]
+            one_hot_masks[lbl_idx, x, y] = 1.
+        return one_hot_masks
+
+    # - In case theres no other classes besides background (i.e., 0) J = 0.
+    J = 0.
+    R = np.array(R)
+    S = np.array(S)
+    # - Convert the multi-label mask to multiple one-hot masks
+    cls = np.unique(R.astype(np.int16))
+
+    if cls[cls > 0].any():  # <= If theres any classes besides background (i.e., 0)
+        R = _get_one_hot_masks(multi_class_mask=R, classes=cls)
+        S = _get_one_hot_masks(multi_class_mask=S, classes=cls)
+
+        # - Calculate the intersection of R and S
+        I_sums = np.sum(R[:, np.newaxis, ...] * S[np.newaxis, ...], axis=(-2, -1))
+        x, y = np.arange(len(I_sums)), np.argmax(I_sums, axis=1)  # <= Choose the once that have the largest overlap with the ground truth label
+        I = I_sums[x, y]
+
+        # - Calculate the union of R and S
+        U_sums = np.sum(np.logical_or(R[:, np.newaxis, ...], S[np.newaxis, ...]), axis=(-2, -1))
+        U = U_sums[x, y]
+
+        # - Mean Jaccard on the valid items only
+        U[U <= 0] = 1  # <= To avoid division by 0
+        J = (I / U)
+
+        # - Calculate the areas of the reference items
+        R_areas = R.sum(axis=(-2, -1))
+        R_areas[R_areas <= 0] = 1  # <= To avoid division by 0
+
+        # - Find out the indices of the items which do not satisfy |I| / |R| > 0.5 and replace them with 0
+        inval = np.argwhere((I / R_areas) <= .5).reshape(-1)
+
+        J[inval] = np.nan
+
+        J = np.nan_to_num(J, copy=True, nan=0.0, posinf=0.0, neginf=0.0)
+
+    return J if isinstance(J, float) else J.mean()
 
 
 def get_files_from_metadata(root_dir: str or pathlib.Path, metadata_files_regex, logger: logging.Logger = None):
@@ -144,7 +168,7 @@ def scan_files(root_dir: pathlib.Path or str, seg_dir_postfix: str, image_prefix
     return file_tuples
 
 
-def get_data_loaders(data_dir: str or pathlib.Path, batch_size: int, train_augs, val_augs, test_augs, seg_dir_postfix: str = SEG_DIR_POSTFIX, image_prefix: str = IMAGE_PREFIX, seg_prefix: str = SEG_PREFIX, val_prop: float = .2, logger: logging.Logger = None):
+def get_data_loaders(data_dir: str or pathlib.Path, train_batch_size: int, train_augs, val_augs, test_augs, seg_dir_postfix: str = SEG_DIR_POSTFIX, image_prefix: str = IMAGE_PREFIX, seg_prefix: str = SEG_PREFIX, val_prop: float = .2, logger: logging.Logger = None):
     fls = scan_files(
         root_dir=data_dir,
         seg_dir_postfix=seg_dir_postfix,
@@ -159,8 +183,8 @@ def get_data_loaders(data_dir: str or pathlib.Path, batch_size: int, train_augs,
         # - Create the DataLoader object
         train_dl = DataLoader(
             ImageDS(file_tuples=train_fls, augs=train_augs()),
-            batch_size=batch_size,
-            num_workers=2,
+            batch_size=train_batch_size,
+            num_workers=NUM_WORKERS,
             pin_memory=True,
             shuffle=True
         )
@@ -168,16 +192,16 @@ def get_data_loaders(data_dir: str or pathlib.Path, batch_size: int, train_augs,
         if val_fls is not None and len(val_fls):
             val_dl = DataLoader(
                 ImageDS(file_tuples=val_fls, augs=val_augs()),
-                batch_size=1,
-                num_workers=2,
+                batch_size=VAL_BATCH_SIZE,
+                num_workers=NUM_WORKERS,
                 pin_memory=True,
                 shuffle=True
             )
     elif test_augs is not None:
         test_dl = DataLoader(
             ImageDS(file_tuples=fls, augs=test_augs()),
-            batch_size=1,
-            num_workers=2,
+            batch_size=TEST_BATCH_SIZE,
+            num_workers=NUM_WORKERS,
             pin_memory=True,
             shuffle=True
         )

tensor_flow/utils/aux_funcs.py
@@ -200,29 +200,29 @@ def get_train_val_idxs(n_items, val_prop):
     return train_idxs, val_idxs
 
 
-def get_one_hot_masks(multi_class_mask: np.ndarray, classes: np.ndarray = None):
-    """
-    Converts a multi-class label into a one-hot labels for each object in the multi-class label
-    :param: multi_class_mask - mask where integers represent different objects
-    """
-    # - Ensure the multi-class label is populated with int values
-    mlt_cls_mask = multi_class_mask.astype(np.int16)
-
-    # - Find the classes
-    cls = classes
-    if cls is None:
-        cls = np.unique(mlt_cls_mask)
-
-    # - Discard the background (0)
-    cls = cls[cls > 0]
-
-    one_hot_masks = np.zeros((len(cls), *mlt_cls_mask.shape), dtype=np.float32)
-    for lbl_idx, lbl in enumerate(one_hot_masks):
-        # for obj_mask_idx, _ in enumerate(lbl):
-        idxs = np.argwhere(mlt_cls_mask == cls[lbl_idx])
-        x, y = idxs[:, 0], idxs[:, 1]
-        one_hot_masks[lbl_idx, x, y] = 1.
-    return one_hot_masks
+# def get_one_hot_masks(multi_class_mask: np.ndarray, classes: np.ndarray = None):
+#     """
+#     Converts a multi-class label into a one-hot labels for each object in the multi-class label
+#     :param: multi_class_mask - mask where integers represent different objects
+#     """
+#     # - Ensure the multi-class label is populated with int values
+#     mlt_cls_mask = multi_class_mask.astype(np.int16)
+#
+#     # - Find the classes
+#     cls = classes
+#     if cls is None:
+#         cls = np.unique(mlt_cls_mask)
+#
+#     # - Discard the background (0)
+#     cls = cls[cls > 0]
+#
+#     one_hot_masks = np.zeros((len(cls), *mlt_cls_mask.shape), dtype=np.float32)
+#     for lbl_idx, lbl in enumerate(one_hot_masks):
+#         # for obj_mask_idx, _ in enumerate(lbl):
+#         idxs = np.argwhere(mlt_cls_mask == cls[lbl_idx])
+#         x, y = idxs[:, 0], idxs[:, 1]
+#         one_hot_masks[lbl_idx, x, y] = 1.
+#     return one_hot_masks
 
 
 def calc_jaccard(R: np.ndarray, S: np.ndarray):
@@ -231,15 +231,40 @@ def calc_jaccard(R: np.ndarray, S: np.ndarray):
     :param: R - Reference multi-class label
     :param: S - Segmentation multi-class label
     """
+    def _get_one_hot_masks(multi_class_mask: np.ndarray, classes: np.ndarray = None):
+        """
+        Converts a multi-class label into a one-hot labels for each object in the multi-class label
+        :param: multi_class_mask - mask where integers represent different objects
+        """
+        # - Ensure the multi-class label is populated with int values
+        mlt_cls_mask = multi_class_mask.astype(np.int16)
+
+        # - Find the classes
+        cls = classes
+        if cls is None:
+            cls = np.unique(mlt_cls_mask)
+
+        # - Discard the background (0)
+        cls = cls[cls > 0]
+
+        one_hot_masks = np.zeros((len(cls), *mlt_cls_mask.shape), dtype=np.float32)
+        for lbl_idx, lbl in enumerate(one_hot_masks):
+            # for obj_mask_idx, _ in enumerate(lbl):
+            idxs = np.argwhere(mlt_cls_mask == cls[lbl_idx])
+            x, y = idxs[:, 0], idxs[:, 1]
+            one_hot_masks[lbl_idx, x, y] = 1.
+        return one_hot_masks
+
     # - In case theres no other classes besides background (i.e., 0) J = 0.
     J = 0.
-
+    R = np.array(R)
+    S = np.array(S)
     # - Convert the multi-label mask to multiple one-hot masks
     cls = np.unique(R.astype(np.int16))
 
     if cls[cls > 0].any():  # <= If theres any classes besides background (i.e., 0)
-        R = get_one_hot_masks(multi_class_mask=R, classes=cls)
-        S = get_one_hot_masks(multi_class_mask=S, classes=cls)
+        R = _get_one_hot_masks(multi_class_mask=R, classes=cls)
+        S = _get_one_hot_masks(multi_class_mask=S, classes=cls)
 
         # - Calculate the intersection of R and S
         I_sums = np.sum(R[:, np.newaxis, ...] * S[np.newaxis, ...], axis=(-2, -1))
@@ -263,11 +288,9 @@ def calc_jaccard(R: np.ndarray, S: np.ndarray):
 
         J[inval] = np.nan
 
-        J = np.nanmean(J)
-
         J = np.nan_to_num(J, copy=True, nan=0.0, posinf=0.0, neginf=0.0)
 
-    return J
+    return J if isinstance(J, float) else J.mean()
 
 
 def check_dir(dir_path: pathlib.Path):